262087518fe4037c3881a6a74040116d75c2fd98
khanalasmit <asmitpasa2063@gmail.com>
2025-10-05 16:22:59 +0545

    cached the data

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-import streamlit as st 
-import pickle 
-from sklearn.metrics import roc_auc_score,auc,roc_curve
+import streamlit as st
+import pickle
+from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from tested_model import OOFStackingClassifier
 
-Kepler_data=pd.read_csv('Training_data.csv')
-X=Kepler_data.drop(columns=['koi_disposition'])
-y=Kepler_data['koi_disposition']
+st.set_page_config(layout='wide')
+st.markdown("<h1 style='text-align: center;'>A Extra Terrestrial Planet Prediction Model</h1>", unsafe_allow_html=True)
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+@st.cache_data
+def load_data():
+    Kepler_data = pd.read_csv('Training_data.csv')
+    X = Kepler_data.drop(columns=['koi_disposition'])
+    y = Kepler_data['koi_disposition']
+    return X, y
 
-with open(r'Stacked.pkl', 'rb') as f:
-    stack_clf = pickle.load(f)
+@st.cache_resource
+def load_model():
+    with open('Stacked.pkl', 'rb') as f:
+        model = pickle.load(f)
+    return model
 
-y_pred_proba=stack_clf.predict_proba(X_test)[:,1]
-# Compute ROC curve and AUC
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+# Load once
+X, y = load_data()
+stack_clf = load_model()
+
+# Train/test split (fast enough)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+# Predict
+y_pred_proba = stack_clf.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
 roc_auc = auc(fpr, tpr)
 
 # Plot ROC
@@ -29,9 +45,4 @@ ax.set_ylabel('True Positive Rate')
 ax.set_title('Receiver Operating Characteristic')
 ax.legend()
 
-st.set_page_config(layout='wide')
-st.markdown("<h1 style='text-align: center;'>A Extra Teresterial Planet prediction model</h1>", unsafe_allow_html=True)
-# Display in Streamlit
 st.pyplot(fig)
-
-
