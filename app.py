cd5806eec2a2953069238473697311cd220b3bb3
khanalasmit <asmitpasa2063@gmail.com>
2025-11-28 13:57:53 +0545

    some changes

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ page = st.sidebar.radio(
 
 # ---------------- Shared Train/Test Split ----------------
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=21, stratify=y
+    X, y, test_size=0.2, random_state=42, stratify=y
 )
 
 # ==========================================================
